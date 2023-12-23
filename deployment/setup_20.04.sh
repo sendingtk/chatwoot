6d3f5e7f9852e4f6ebfc7ca16b661a6b0582feb4
rotsen <nestordavalos@gmail.com>
2023-12-22 21:33:34 -0300

    fix: correcion nginx_chatwoot.conf

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -415,7 +415,7 @@ function setup_ssl() {
     echo "debug: letsencrypt email: $le_email"
   fi
   curl https://ssl-config.mozilla.org/ffdhe4096.txt >> /etc/ssl/dhparam
-  wget https://raw.githubusercontent.com/sendingtk/chatwoot/develop/deployment/nginx_chatwoot.conf
+  wget https://raw.githubusercontent.com/sendingtk/chatwoot/master/deployment/nginx_chatwoot.conf
   cp nginx_chatwoot.conf /etc/nginx/sites-available/nginx_chatwoot.conf
   certbot certonly --non-interactive --agree-tos --nginx -m "$le_email" -d "$domain_name"
   sed -i "s/chatwoot.domain.com/$domain_name/g" /etc/nginx/sites-available/nginx_chatwoot.conf
