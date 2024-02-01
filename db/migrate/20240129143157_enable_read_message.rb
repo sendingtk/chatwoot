1f55cca9cd7afd7300004b7da52fb4e6c875ecd6
nestordavalos <nestordavalos@gmail.com>
2024-02-01 19:28:44 -0300

    update

--- db/migrate/20240129143157_enable_read_message.rb
+++ db/migrate/20240129143157_enable_read_message.rb
@@ -1,10 +1,10 @@
 class EnableReadMessage < ActiveRecord::Migration[6.1]
-    def change
-      Account.find_in_batches do |account_batch|
-        account_batch.each do |account|
-          account.enable_features('read_message')
-          account.save!
-        end
+  def change
+    Account.find_in_batches do |account_batch|
+      account_batch.each do |account|
+        account.enable_features('read_message')
+        account.save!
       end
     end
   end
+end
