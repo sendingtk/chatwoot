b3083488b0f148f847630d49ffa3a9f0fc99720a
nestordavalos <nestordavalos@gmail.com>
2024-04-09 15:56:38 -0400

    fix: feature agent viewed

--- app/jobs/update_last_seen_job.rb
+++ app/jobs/update_last_seen_job.rb
@@ -3,25 +3,33 @@ class UpdateLastSeenJob < ApplicationJob
 
   def perform(conversation_id, user, agent_last_seen_at)
     conversation = Conversation.find(conversation_id)
-    
-    key = "conversation:last_seen:#{conversation_id}:#{user.id}"
-    unless ::Redis::Alfred.get(key)
-      ::Redis::Alfred.setex(key, true, 1.hour)
-      params = {
-        message_type: :activity,
-        content_type: :text,
-        content: "Conversa visualizada por #{user.id} - #{user.name}"
-      }
-      Messages::MessageBuilder.new(user, conversation, params).perform
-    end
 
-    return unless conversation.account.feature_enabled?('read_message')
-    
+    agent_viewed(conversation, user) if conversation.account.feature_enabled?('agent_conversation_viewed')
+
+    read_message(conversation, agent_last_seen_at) if conversation.account.feature_enabled?('read_message')
+  end
+
+  private
+
+  def read_message(conversation, agent_last_seen_at)
     messages = conversation.messages.to_read(agent_last_seen_at)
-    Rails.logger.debug { "Conversation #{conversation_id} with #{messages.size} message(s) to update status to read" }
+    Rails.logger.debug { "Conversation #{conversation.id} with #{messages.size} message(s) to update status to read" }
     messages.each do |message|
       Rails.logger.debug { "Update message id #{message.id} source #{message.source_id} status to read" }
       message.update(status: :read)
     end
+  end
+
+  def agent_viewed(conversation, user)
+    key = "conversation:last_seen:#{conversation.id}:#{user.id}"
+    return if ::Redis::Alfred.get(key)
+
+    ::Redis::Alfred.setex(key, true, 1.hour)
+    params = {
+      message_type: :activity,
+      content_type: :text,
+      content: "Conversa visualizada por #{user.id} - #{user.name}"
+    }
+    Messages::MessageBuilder.new(user, conversation, params).perform
   end
 end
