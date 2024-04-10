3279d7ef9192c0684558b25ddc18f02b8dd7aeac
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-04-10 17:01:34 -0300

    change file type

--- app/services/notifica_me/send_on_notifica_me_service.rb
+++ app/services/notifica_me/send_on_notifica_me_service.rb
@@ -9,6 +9,7 @@ class NotificaMe::SendOnNotificaMeService < Base::SendOnChannelService
     begin
       url = "https://hub.notificame.com.br/v1/channels/#{channel.notifica_me_type}/messages"
       body = message_params.to_json
+      Rails.logger.error("NotificaMe message params #{body}")
       response = HTTParty.post(
         url,
         body: body,
@@ -51,10 +52,11 @@ class NotificaMe::SendOnNotificaMeService < Base::SendOnChannelService
 
   def message_params_media
     message.attachments.map { |a|
+      file_type = file_type(a)
       data = {
         type: :file,
-        fileMimeType: a.file_type,
-        fileUrl: a.download_url,
+        fileMimeType: file_type,
+        fileUrl: a.download_url
       }
       if message.content
         data[:fileCaption] = message.content
@@ -70,5 +72,26 @@ class NotificaMe::SendOnNotificaMeService < Base::SendOnChannelService
 
   def channel
     @channel ||= inbox.channel
+  end
+
+  def file_type(attachment)
+    if attachment.file_type == 'image'
+      return 'photo' if channel.notifica_me_type == 'telegram'
+    elsif attachment.file_type == 'file'
+      extension = extension(attachment.download_url)
+      if ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'odt', 'csv', 'txt'].include?(extension)
+        return 'document'
+      elsif attachment.file_type == 'file' && ['mov', 'mp4'].include?(extension)
+        return 'video'
+      elsif attachment.file_type == 'file' && ['ogg', 'mp3', 'wav'].include?(extension)
+        return 'audio'
+      end
+    end
+    return attachment.file_type
+  end
+
+  def extension(url)
+    split = url.split('.')
+    split[split.length - 1]
   end
 end
