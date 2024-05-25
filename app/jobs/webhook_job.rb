17fa8677f5608176e7a09b0de4a85a58ce707f9e
nestordavalos <nestordavalos@gmail.com>
2024-05-25 11:47:20 -0400

    fix(typo): Update bulkActions.json feat: Adds backend support for rendering tables in articles

--- app/jobs/webhook_job.rb
+++ app/jobs/webhook_job.rb
@@ -1,8 +1,7 @@
 class WebhookJob < ApplicationJob
   queue_as :medium
-
+  #  There are 3 types of webhooks, account, inbox and agent_bot
   def perform(url, payload, webhook_type = :account_webhook, method = :post, headers = { content_type: :json, accept: :json })
-    #  There are 3 types of webhooks, account, inbox and agent_bot
     Webhooks::Trigger.execute(url, payload, webhook_type, method, headers)
   end
 end
