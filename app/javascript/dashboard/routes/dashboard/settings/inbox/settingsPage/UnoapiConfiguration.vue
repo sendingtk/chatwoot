e7bc630987f46b9276b489d647e872f1874eef13
nestordavalos <nestordavalos@gmail.com>
2024-06-25 23:59:15 -0400

    fix

--- app/javascript/dashboard/routes/dashboard/settings/inbox/settingsPage/UnoapiConfiguration.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/settingsPage/UnoapiConfiguration.vue
@@ -7,7 +7,7 @@
         <input
           v-model.trim="url"
           type="text"
-          placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.URL.PLACEHOLDER')"
+          :placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.URL.PLACEHOLDER')"
         />
         <span v-if="$v.url.$error" class="message">
           {{ $t('INBOX_MGMT.ADD.WHATSAPP.URL.ERROR') }}
