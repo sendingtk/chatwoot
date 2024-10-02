2495a2d471452a88190d2292f280c2493514c7c5
nestordavalos <nestordavalos@gmail.com>
2024-10-01 22:30:05 -0400

    fix: Corregir error en la obtención del token de autorización de UNOAPI en Whatsapp::UnoapiWebhookSetupService

--- app/services/whatsapp/unoapi_webhook_setup_service.rb
+++ app/services/whatsapp/unoapi_webhook_setup_service.rb
@@ -65,7 +65,7 @@ class Whatsapp::UnoapiWebhookSetupService
 
   def headers(whatsapp_channel)
     {
-      Authorization: ENV.fetch('UNOAPI_AUTH_TOKEN', whatsapp_channel.provider_config['api_key']),
+      Authorization: GlobalConfigService.load('UNOAPI_AUTH_TOKEN', whatsapp_channel.provider_config['api_key']),
       'Content-Type': 'application/json'
     }
   end
