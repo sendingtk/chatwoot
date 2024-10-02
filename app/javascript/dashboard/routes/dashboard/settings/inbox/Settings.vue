e8d4627aadc57e8849f72c4a1608a9a8dbd67c6b
nestordavalos <nestordavalos@gmail.com>
2024-10-01 22:52:39 -0400

    fix: Corregir error en la obtención del token de autorización de UNOAPI en Whatsapp::UnoapiWebhookSetupService
    - Se corrigió un error en la obtención del token de autorización de UNOAPI en el servicio de configuración de webhook de Whatsapp. El problema ha sido solucionado.
    
    fix: Corregir error en la traducción de "La conversación fue marcada como resulta repor"
    
    - Se corrigió un error en la traducción de la frase "La conversación fue marcada como resulta repor". Ahora la traducción es correcta.
    
    fix: Twilio channel attachment issues
    
    - Se solucionaron problemas relacionados con la carga de imágenes desde S3 externo en el chat del canal de Twilio.
    
    fix: bug with loading images from external S3 in chat
    
    - Se corrigió un error relacionado con la carga de imágenes desde S3 externo en el chat.
    
    feat: Agregar soporte para Docker Compose en el flujo de trabajo de publicación de la imagen de Codespace
    
    - Se agregó soporte para Docker Compose en el flujo de trabajo de publicación de la imagen de Codespace. Ahora es posible utilizar Docker Compose para gestionar la imagen de Codespace.

--- app/javascript/dashboard/routes/dashboard/settings/inbox/Settings.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/Settings.vue
@@ -7,12 +7,13 @@ import SettingIntroBanner from 'dashboard/components/widgets/SettingIntroBanner.
 import SettingsSection from '../../../../components/SettingsSection.vue';
 import inboxMixin from 'shared/mixins/inboxMixin';
 import FacebookReauthorize from './facebook/Reauthorize.vue';
+import MicrosoftReauthorize from './channels/microsoft/Reauthorize.vue';
+import GoogleReauthorize from './channels/google/Reauthorize.vue';
 import PreChatFormSettings from './PreChatForm/Settings.vue';
 import WeeklyAvailability from './components/WeeklyAvailability.vue';
 import GreetingsEditor from 'shared/components/GreetingsEditor.vue';
 import ConfigurationPage from './settingsPage/ConfigurationPage.vue';
 import CollaboratorsPage from './settingsPage/CollaboratorsPage.vue';
-import MicrosoftReauthorize from './channels/microsoft/Reauthorize.vue';
 import WidgetBuilder from './WidgetBuilder.vue';
 import BotConfiguration from './components/BotConfiguration.vue';
 import UnoapiConfiguration from './settingsPage/UnoapiConfiguration.vue';
@@ -34,6 +35,7 @@ export default {
     SenderNameExamplePreview,
     MicrosoftReauthorize,
     UnoapiConfiguration,
+    GoogleReauthorize,
   },
   mixins: [inboxMixin],
   setup() {
@@ -221,7 +223,15 @@ export default {
     facebookUnauthorized() {
       return this.isAFacebookInbox && this.inbox.reauthorization_required;
     },
+    googleUnauthorized() {
+      const isLegacyInbox = ['imap.gmail.com', 'imap.google.com'].includes(
+        this.inbox.imap_address
+      );
 
+      return (
+        (this.isAGoogleInbox || isLegacyInbox) &&
+        this.inbox.reauthorization_required
+      );
     isWavoipFeatureEnabled() {
       return this.isFeatureEnabledonAccount(this.accountId, FEATURE_FLAGS.WAVOIP);
     },
@@ -395,9 +405,9 @@ export default {
         />
       </woot-tabs>
     </SettingIntroBanner>
-
     <MicrosoftReauthorize v-if="microsoftUnauthorized" :inbox="inbox" />
     <FacebookReauthorize v-if="facebookUnauthorized" :inbox="inbox" />
+    <GoogleReauthorize v-if="googleUnauthorized" :inbox="inbox" />
 
     <div v-if="selectedTabKey === 'inbox_settings'" class="mx-8">
       <SettingsSection
